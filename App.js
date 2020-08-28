676a1222289f48272c86e81d398d6ff9f87d920e
Pongsathorn <70096745+Pongsathorn88@users.noreply.github.com>
2020-08-28 14:28:30 +0700

    Add files via upload

--- App.js
+++ App.js
@@ -1,62 +1,16 @@
-import React ,{ useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import {
-  Collapse,
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink,
-  UncontrolledDropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem,
-  NavbarText
-} from 'reactstrap';
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Table from './Table';
 
-const Example = (props) => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggle = () => setIsOpen(!isOpen);
-
-  return (
-    <div>
-      <Navbar color="light" light expand="md">
-        <NavbarBrand href="/"><img src="logo.jpg"></img></NavbarBrand>
-        <NavbarToggler onClick={toggle} />
-        <Collapse isOpen={isOpen} navbar>
-          <Nav className="mr-auto" navbar>
-            <NavItem>
-              <NavLink href="http://www.iii-logistics.com/">Home</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="/components/">Booking</NavLink>
-            </NavItem>
-            <UncontrolledDropdown nav inNavbar>
-              <DropdownToggle nav caret>
-                Operation
-              </DropdownToggle>
-              <DropdownMenu right>
-                <DropdownItem>
-                  Document Cargo
-                </DropdownItem>
-                <DropdownItem>
-                  X-RAY
-                </DropdownItem>
-                <DropdownItem divider />
-                <DropdownItem>
-                  Reset
-                </DropdownItem>
-              </DropdownMenu>
-            </UncontrolledDropdown>
-          </Nav>
-          <NavbarText>TRIPLE i</NavbarText>
-        </Collapse>
-      </Navbar>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <Table />
+      </div>
+    )
+  }
 }
 
-export default Example;
+export default App;
