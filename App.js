27bfb4d5e9ab5f6f16f7ac6d028d50d69ed23a8f
Pongsathorn <70096745+Pongsathorn88@users.noreply.github.com>
2020-08-28 19:50:33 +0700

    Update App.js

--- App.js
+++ App.js
@@ -2,15 +2,39 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Table from './Table';
+import {Buuton} from 'antd';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="container">
-        <Table />
-      </div>
-    )
+  state = {
+    characters  
+      {
+        name: 'Charlie',
+        job: 'Janitor',
+      },
+      {
+        name: 'Mac',
+        job: 'Bouncer',
+      },
+      {
+        name: 'Dee',
+        job: 'Aspring actress',
+      },
+      {
+        name: 'Dennis',
+        job: 'Bartender',
+      },
+    ],
   }
+}
+
+
+render () {
+  const { characters } = this.state
+  return (
+    <div className="container">
+      <Table characterData={characters} removeCharacter={this.removeCharacter} />
+    </div>
+  )
 }
 
 export default App;
