319ed797ffad7beeb71f6986a384ad968db9de38
Pongsathorn <70096745+Pongsathorn88@users.noreply.github.com>
2020-08-28 19:51:12 +0700

    Update Table.js

--- Table.js
+++ Table.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react'
+import {Buuton} from 'antd';
 
 const TableHeader = () => {
   return (
@@ -10,39 +11,34 @@ const TableHeader = () => {
         </thead>
   )
 }
-const TableBody = () => {
-  return (
-        <tbody>
-          <tr>
-            <td>Charlie</td>
-            <td>Janitor</td>
-          </tr>
-          <tr>
-            <td>Mac</td>
-            <td>Bouncer</td>
-          </tr>
-          <tr>
-            <td>Dee</td>
-            <td>Aspiring actress</td>
-          </tr>
-          <tr>
-            <td>Dennis</td>
-            <td>Bartender</td>
-          </tr>
-        </tbody>
+
+const TableBody = (props) => {
+  const rows = props.characterData.map((row, index) => {
+    return (
+      <tr key={index}>
+        <td>{row.name}</td>
+        <td>{row.job}</td>
+        <td>
+    <button onClick={() => props.removeCharacter(index)}>Delete</button>
+  </td>
+      </tr>
     )
-  }
+  })
+
+  return <tbody>{rows}</tbody>
 }
 
 class Table extends Component {
   render() {
+    const {characterData, removeCharacter} = props
+
     return (
       <table>
-        <TableHeader />
-        <TableBody />
-      </table>
+      <TableHeader />
+      <TableBody characterData={characterData} removeCharacter={removeCharacter} />
+    </table>
     )
+    }
   }
-}
 
 export default Table;
